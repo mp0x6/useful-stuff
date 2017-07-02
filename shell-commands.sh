fbfa379eb3a6d914af34ba22cbf67f6067d643c9
mp6 <mp0x6@users.noreply.github.com>
2017-07-02 14:13:36 +0200

    Update shell-commands.sh

--- shell-commands.sh
+++ shell-commands.sh
@@ -1,7 +1,12 @@
-# Nützliche Shellcommands
+# Useful shell commands
 
-## PDF
+# #######################
+# ########  PDF  ########
+# #######################
 
-ps2pdf input.pdf output.pdf	# vereinfacht die Logik der PDF-Dateien, lossless Komprimierung, killt DRM, behebt *keine* Fehler
-gs -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/prepress -sOutputFile=repaired.pdf kaputt.pdf	# rebuilded das PDF, repariert und komprimiert (/prepress lossless, /ebook gute Qualität)
+# converts pdf to ps and back, compresses lossless, does *not* rebuild the pdf (so errors remain), no DRM-deletion
+ps2pdf input.pdf output.pdf
+# rebuilds and repairs, compresses if using /ebook for good quality, /prepress for lossless quality, gets rid of any DRM
+gs -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/prepress -sOutputFile=repaired.pdf kaputt.pdf
+# unites pdf files (but it does not work on DRM'ed files, may fail when encountering invalid PDF files)
 pdfunite datei1.pdf datei2.pdf datei3.pdf output.pdf
