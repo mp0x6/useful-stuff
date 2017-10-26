f6d9f2efa0acdcbad6fc688ed85aa1977042ab43
mp0x6 <mp0x6@users.noreply.github.com>
2017-10-26 21:42:25 +0200

    correct error in description of Ghostscript

--- shell-commands.sh
+++ shell-commands.sh
@@ -6,7 +6,7 @@
 
 # converts pdf to ps and back, compresses lossless, does *not* rebuild the pdf (so errors remain), no DRM-deletion
 ps2pdf input.pdf output.pdf
-# rebuilds and repairs, compresses if using /ebook for good quality, /prepress for lossless quality, gets rid of any DRM
+# rebuilds and repairs, compresses if using /ebook for good quality, /prepress for lossless quality, gets rid of some DRM. If you need more, use Calibre with plugins.
 gs -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/prepress -sOutputFile=repaired.pdf defect.pdf
 # unites pdf files (but it does not work on DRM'ed files, may fail when encountering invalid PDF files)
 pdfunite datei1.pdf datei2.pdf datei3.pdf output.pdf
